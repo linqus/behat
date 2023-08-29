58d91e7990d8a2e3b2b9e31f5f62fb849957e865
Karol Linkiewicz <karol@linkiewicz.pl>
2023-08-29 09:21:54 +0200

    ls feature

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -9,7 +9,7 @@ use Behat\MinkExtension\Context\MinkContext;
 /**
  * Defines application features from the specific context.
  */
-class FeatureContext extends MinkContext implements Context, SnippetAcceptingContext
+class FeatureContext implements Context, SnippetAcceptingContext
 {
     /**
      * Initializes context.
@@ -18,7 +18,36 @@ class FeatureContext extends MinkContext implements Context, SnippetAcceptingCon
      * You can also pass arbitrary arguments to the
      * context constructor through behat.yml.
      */
+
+    private string $output;
+
     public function __construct()
     {
+    }
+
+    /**
+     * @Given I have a file named :file
+     */
+    public function iHaveAFileNamed($file)
+    {
+        touch($file);
+    }
+
+    /**
+     * @When I run :command
+     */
+    public function iRun($command)
+    {
+        $this->output = shell_exec($command);
+    }
+
+    /**
+     * @Then I should see :string in the output
+     */
+    public function iShouldSeeInTheOutput($string)
+    {
+        if (!strpos($this->output, $string )) {
+            throw new Exception(printf("No %s found in %s", $string, $this->output));
+        }
     }
 }
