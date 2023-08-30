b62cefc5a3567fa279620e34a459075361627779
Karol Linkiewicz <karol@linkiewicz.pl>
2023-08-30 16:43:57 +0200

    new page with form

--- src/AppBundle/Controller/ProductAdminController.php
+++ src/AppBundle/Controller/ProductAdminController.php
@@ -7,6 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductAdminController extends Controller
 {
@@ -21,6 +22,34 @@ class ProductAdminController extends Controller
 
         return $this->render('product/list.html.twig', [
             'products' => $products
+        ]);
+    }
+
+    /**
+     * @Route("/admin/products/new", name="new_product")
+     */
+    public function newProduct(Request $request)
+    {
+        $message = '';
+        if ($request->isMethod('POST')) {
+            $this->addFlash('success',"Product created FTW!");
+
+            $product = new Product();
+            $product->setName($request->get('name'));
+            $product->setDescription($request->get('description'));
+            $product->setPrice($request->get('price'));
+
+            $product->setAuthor($this->getUser());
+
+            $em = $this->getDoctrine()->getEntityManager();
+            $em->persist($product);
+            $em->flush();
+            
+            return $this->redirectToRoute('product_list');
+        }
+
+        return $this->render('product/new.html.twig',[
+            'message' => $message,
         ]);
     }
 }
